464ccf5564e10c65d30e10a5cbd87afb535526e3
Mohini-svt <your_github_emohinikumari@gmail.com>
2025-12-27 20:39:27 +0530

    Add Day 2: conditional statements

---
+++ Day 2.py
@@ -0,0 +1,27 @@
+# Grade Calculator program 
+
+name = input("Enter your name please:")
+print("Hello" + name)
+
+marks = int(input("Enter your marks please:"))
+print(marks)
+
+if marks >= 60:
+    print("You have got an A grade.")
+
+elif marks >= 50:
+    print("You have gotten a B grade.")
+
+elif marks >= 40:
+    print("You have gotten a C grade.")
+
+elif marks >= 30:
+    print("You have gotten a D grade.")
+
+
+else:
+    print("You have failed the exam.")
+
+    
+
+ 
